be7ce9951b46a3963a0303f7fc1a41cd78628b8a
Wezley <wezley.singleton@gmail.com>
2020-04-16 14:16:39 -0400

    reimplement for demo

--- src/util/mail-worker.js
+++ src/util/mail-worker.js
@@ -9,23 +9,15 @@ MailWorker.prototype.__proto__ = events.EventEmitter.prototype;
 
 const mailWorker = new MailWorker();
 
-mailWorker.on('newRegister', function(recipient) {
+mailWorker.on('newRegister', function(recipientEmail) {
 
-    console.log(`Sending a welcome email to ${recipient} using SMTP server at ${this.server} on port ${this.port}`);
-    
+    console.log(`Sending a welcome email to ${recipientEmail} using SMTP server at ${this.server} on port ${this.port}`);
+
     // pretend to send a message
     setTimeout(() => {
-        console.log('Message successfully sent');
+        console.log('Message successfully sent!');
     }, 1000);
 
 });
-
-// USE THIS FIRST TO DEMONSTRATE 'this' BINDING
-// mailWorker.on('newRegister', recipient => {
-//     console.log(`Sending a welcome email to ${recipient} using SMTP server at ${this.server} on port ${this.port}`);
-//     setTimeout(() => {
-//         console.log('Message successfully sent');
-//     }, 1000);
-// });
 
 module.exports = mailWorker;
