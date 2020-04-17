a80359464fa6b413d5af7090f1dce5fa9091cb57
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-16 23:22:53 -0400

    added a method to retrieved a user by id of 0 and an id of 1

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const userRepo = require('./src/repos/user-repo');
 
-userRepo.getInstance().getAllUsers((err, result) => {
-    err && console.log(err);
-    result && console.log(result);
-});
+// userRepo.getInstance().getAllUsers((err, result) => {
+//     err && console.log(err);
+//     result && console.log(result);
+// });
+
+let result = userRepo.getInstance().getUserById(0);
+let result2 = userRepo.getInstance().getUserById(1);
+//console.log(result);
+result(response => {
+    console.log(response);
+    result2(response => {
+        console.log(response);
+    })
+})
