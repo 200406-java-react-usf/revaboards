d633de78ad5d0856f86b4daa63d4d4bbd993215a
KaneishaR <kaneisha.reese@gmail.com>
2020-04-19 21:19:52 -0500

    added thunks to current posts funtcions

--- revaboards/index.js
+++ revaboards/index.js
@@ -1,6 +1,27 @@
-const userRepo = require('./src/repos/user-repo');
+//const userRepo = require('./src/repos/user-repo');
+const postRepo = require('./src/repos/post-repo');
+const Post = require('./src/models/post');
 
-userRepo.getInstance().getAllUsers((err, result) => {
-    err && console.log(err);
-    result && console.log(result);
+// let task1 = postRepo.getInstance().getPostById(1);
+
+// task1(text =>{
+//     console.log(text);
+// });
+
+// let task2 = postRepo.getInstance().getPostsByPosterId(2);
+
+// task2(text =>{
+//     console.log(...text);
+// });
+
+let testPost = new Post(1, 'This is a test', 'Testing out the new add posts feature with thunks', 2);
+let task3 = postRepo.getInstance().addPost(testPost);
+
+task3(text =>{
+    console.log(...text);
 });
+
+// userRepo.getInstance().getAllUsers((err, result) => {
+//     err && console.log(err);
+//     result && console.log(result);
+// });
--- revaboards/src/repos/post-repo.js
+++ revaboards/src/repos/post-repo.js
@@ -1,32 +1,106 @@
 // implement and export the methods: getPostById and getPostsByPosterId
 const postData = require('../data/post-db');
-module.exports = (function(){
+module.exports = (function() {
     
     let instance;
-    
+   
     function init() {
-        const getPostById = (id, cb) => {
+        // const getPostById = (id, cb) => {
+        //     setTimeout(() => {
+        //         if (!id || id <= 0) throw new Error('Oh no! You gave me bad data!');
+        //         const post = postData.filter(post => post.id === id).pop();
+        //         if (!post) throw new Error('No post found with provided id!');
+        //         cb(post);
+        //     }, 250);
+        // }
+        
+        function getPostById(id) {
+            let called;
+            let thunkCb;
             setTimeout(() => {
-                if (!id || id <= 0) throw new Error('Oh no! You gave me bad data!');
+               
+                if (!id || id <= 0){
+                    thunkCb ='Oh no! You gave me bad data!';
+                }
                 const post = postData.filter(post => post.id === id).pop();
-                if (!post) throw new Error('No post found with provided id!');
-                cb(post);
+                if (!post) {
+                    thunkCb =('No post found with provided id!');
+                }
+                console.log(post)
+                thunkCb = post;
+                
             }, 250);
+            return function(cb) {
+                
+                if (!called) {
+                    called = true;
+                    thunkCb = cb
+                }
+                else{
+                    cb(post)
+                }
+            }
         }
 
-        const getPostsByPosterId = (posterId, cb) => {
+        function getPostsByPosterId(posterId) {
+            let called;
+            let thunkCb;
             setTimeout(() => {
-                const posts = postData.filter(post => post.posterId === posterId);
-                cb(posts);
-            }, 250);
-        }
+                //console.log('1')
+                if (!posterId || posterId <= 0){
+                    thunkCb ='Oh no! You gave me bad data!';
+                }
 
-        const addPost = function(title, body, posterId){
-            console.log(postData.length);
-            let id = null;
-            id = postData.length +1;
-            postData.push(new Post(id, title, body, posterId));
-            console.log(postData);
+                const posts = postData.filter(post => post.poster == posterId);
+                if (posts.length == 0) {
+                    thunkCb =('No posts found with provided poster id!');
+                }
+                else{
+                    thunkCb = posts;
+                }
+            }, 250);
+            return function(cb) {
+                //console.log('3')
+                if (!called) {
+                    called = true;
+                    thunkCb = cb
+                   // console.log('4')
+                }
+                else{
+                   // console.log('5')
+                    cb(posts)
+                }
+            }
+        }
+        
+
+        function addPost (newPost){
+           let called;
+           let thunkCb;
+            if(!newPost){
+                thunkCb = 'Falsy post object provided.'
+            }
+
+            setTimeout(() => {
+                console.log(postData.length)
+                newPost.id = (postData.length) + 1;
+                postData.push(newPost);
+                console.log(postData.length)
+                thunkCb = postData;
+        
+            }, 250);
+
+            return function(cb) {
+                
+                if (!called) {
+                    called = true;
+                    thunkCb = cb
+                }
+                else{
+                    cb(postData)
+                }
+            }
+
         }
 
         return {
@@ -41,4 +115,4 @@ module.exports = (function(){
             return !instance ? instance = init() : instance;
         }
     }
-})
+})();
