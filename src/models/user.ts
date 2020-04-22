4a50bed5428d54af4aa4297b97d033340681d42d
Wagenheim <wagenheimk@me.com>
2020-04-22 17:43:06 -0400

    changed user class to TS

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+export class User {
+
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    dob: string;
+
+    constructor(id: number, un: string, pw: string, fn: string, ln: string, email: string, dob: string) {
+        this.id = id
+        this.username = un;
+        this.password = pw;
+        this.firstName = fn;
+        this.lastName = ln;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    // age() {
+    //     return Math.abs(new Date(Date.now() - this.dob.getTime()) - 1970);
+    // }
+
+} 
