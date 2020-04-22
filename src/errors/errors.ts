8bfda17d6ae596f0d318e463ea0aded73f9ca81c
Wagenheim <wagenheimk@me.com>
2020-04-22 15:56:17 -0400

    changed errors to TS and added one

---
+++ src/errors/errors.ts
@@ -0,0 +1,70 @@
+class ApplicationError {
+
+    message: string;
+    reason: string;
+
+    constructor(rsn?: string) {
+
+        this.message = 'An Unexpected Error occured';
+        rsn ? (this.reason = rsn) : this.reason = 'Unspecified reason.';
+
+    }
+
+    setMessage(message: string){
+        this.message = message;
+    }
+
+}
+
+class ResourcePersistenceError extends ApplicationError {
+
+    constructor (reason?: string) {
+        super(); //invokes Application Error's constructor
+        super.setMessage('Resource not persisted'); 
+    }
+    
+}
+
+class ResourceNotFoundError extends ApplicationError {
+
+    constructor (reason?: string) {
+        super(); //invokes Application Error's constructor
+        super.setMessage('No resource Found'); 
+    }
+    
+}
+
+class BadRequestError extends ApplicationError {
+
+    constructor (reason?: string) {
+        super(); //invokes Application Error's constructor
+        super.setMessage('Invalid Params'); 
+    }
+    
+}
+
+class AuthenticationError extends ApplicationError {
+
+    constructor (reason?: string) {
+        super(); //invokes Application Error's constructor
+        super.setMessage('Authentication failed'); 
+    }
+    
+}
+
+class NotImplementedError extends ApplicationError {
+
+    constructor (reason?: string) {
+        super(); //invokes Application Error's constructor
+        super.setMessage('No implementation yet!'); 
+    }
+    
+}
+
+export {
+    ResourceNotFoundError,
+    ResourcePersistenceError,
+    BadRequestError,
+    AuthenticationError,
+    NotImplementedError
+}
