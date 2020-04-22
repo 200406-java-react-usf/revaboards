f68b46f8170c4f8599d42c7fb5ae2effbcbf9b9e
wezley <wezley.singleton@gmail.com>
2020-04-22 15:49:37 -0400

    converted to TS

--- src/data/post-db.ts
+++ src/data/post-db.ts
@@ -1,7 +1,9 @@
-const Post = require('../models/post');
+import { Post } from '../models/post';
+
+// challenge: replace this with id generator function
 let id = 1;
 
-module.exports = [
+export default [
     new Post(id++, 'hello world!', 'This is my first post! Huzzah!', 1),
     new Post(id++, 'this is cool', 'NodeJS is so awesome! It uses the same JS engine as Google Chrome: V8', 1),
     new Post(id++, 'no limits', 'NodeJS is a runtime environment that lets JS run outside of the browser!', 2),
