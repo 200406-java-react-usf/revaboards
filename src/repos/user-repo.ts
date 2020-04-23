80eb15e4c53df3c7c8767d9cb9b9a97c7f5f3fba
neftaly polanco <polanconeftaly@gmail.com>
2020-04-23 12:52:27 -0400

    eslint added and files changed to TS

---
+++ src/repos/user-repo.ts
@@ -0,0 +1,329 @@
+import data from '../data/user-db';
+import { User } from '../models/user'
+import mailWorker from '../util/mail-worker';
+import { CrudRepository } from './crud-repo';
+import {
+    BadRequestError,
+    ResourceNotFoundError,
+    ResourcePersistenceError,
+    NotImplementedError
+} from '../errors/errors';
+
+
+export class UserReporsitory implements CrudRepository<User> {
+
+    private static instance: UserReporsitory;
+    private constructor() {}
+    static getInstance() {
+        return !UserReporsitory.instance ? UserReporsitory.instance = new UserReporsitory() : UserReporsitory.instance;
+    }
+
+    getAll(): Promise<User[]>{
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let users: User[] = [];
+
+                for(let user of data) {
+                    users.push({...user});
+                }
+
+                if(users.length === 0) {
+                    reject(new ResourceNotFoundError());
+                    return;
+                }
+
+                resolve(users.map(this.removePassword));
+            }, 1000);
+        });
+    }
+
+    getById(id: number): Promise<User>{
+
+        return new Promise<User>((resolve, reject) => {
+            if(typeof id  !== 'number' || !Number.isInteger(id) || id <= 0) {
+                reject(new BadRequestError());
+                return;
+            }
+            setTimeout( () => {
+
+                const myUser = {...data.filter(user => user.id === id)[0]};
+
+                if(!myUser) {
+                    reject(new ResourceNotFoundError());
+                    return;
+                }
+
+                resolve(myUser);
+
+            }, 1000)
+        });
+    }
+
+    save(newUser: User): Promise<User>{
+        return new Promise<User>((resolve, reject) => {
+            if (!newUser) {
+                reject(new BadRequestError('Falsy user object provided.'));
+                return;
+            }
+        
+            let invalid = !Object.keys(newUser).every(key => {
+                if(key == 'id') return true;
+                return newUser[key];
+            });
+        
+            if (invalid) {
+                reject(new BadRequestError('Invalid property values found in provided user.'));
+                return;
+            }
+        
+            setTimeout(() => {
+        
+                let conflict = data.filter(user => user.username == newUser.username).pop();
+        
+                if (conflict) {
+                    reject(new ResourcePersistenceError('The provided username is already taken.'));
+                    return;
+                }
+        
+                conflict = data.filter(user => user.email == newUser.email).pop();
+        
+                if (conflict) {
+                    reject(new ResourcePersistenceError('The provided email is already taken.'));
+                    return;
+                }
+        
+                newUser.id = (data.length) + 1;
+                data.push(newUser);
+
+                mailWorker.emit('newRegister', newUser.email);
+        
+                resolve(newUser);
+
+        });
+        
+    });
+        
+    }
+    Update(): Promise<boolean>{
+    } 
+
+    deleteById(): Promise<Void>{}
+
+    private removePassword (user: User): User {
+        let usr = {...user};
+        delete usr.password;
+        return usr;
+    }
+}
+
+
+
+//  module.exports = (function() {
+
+//     let instance;
+
+//     function init() {
+
+//         const getAllUsers = (cb) => {
+
+//             setTimeout(() => {
+                
+//                 let users = [];
+    
+//                 for (user of data) {
+//                     users.push({...user});
+//                 }
+        
+//                 if (users.length == 0) {
+//                     cb(new errors.ResourceNotFoundError());
+//                     return;
+//                 }
+        
+//                 users = users.map(user => {
+//                     delete user.password;
+//                     return user;
+//                 });
+        
+//                 cb(null, users);
+        
+//             }, 250);
+//         }
+    
+//         const getUserById = (id, cb) => {
+    
+//             if (typeof id !== 'number' || !Number.isInteger(id) || id <= 0) {
+//                 cb(new errors.BadRequestError());
+//                 return;
+//             }
+           
+//             setTimeout(() => {
+        
+//                 const user = {...data.filter(user => user.id === id).pop()};
+                
+//                 if (!user) {
+//                     cb(new errors.ResourceNotFoundError());
+//                     return;
+//                 }
+        
+//                 cb(null, user);
+        
+//             }, 250);
+        
+//         }
+        
+//         const getUserByUsername = (un, cb) => {
+        
+//             if (typeof un !== 'string' || !un) {
+//                 cb(new errors.BadRequestError());
+//                 return;
+//             }
+           
+//             setTimeout(() => {
+        
+//                 const user = {...data.filter(user => user.username === un).pop()};
+                
+//                 if (Object.keys(user).length == 0) {
+//                     cb(new errors.ResourceNotFoundError());
+//                     return;
+//                 }
+        
+//                 cb(null, user);
+        
+//             }, 250);
+        
+//         }
+        
+//         const getUserByCredentials = (un, pw, cb) => {
+        
+//             if (!un || !pw || typeof un !== 'string' || typeof pw !== 'string') {
+//                 cb(new errors.BadRequestError());
+//                 return;
+//             }
+        
+//             setTimeout(() => {
+        
+//                 const user = data.filter(user => user.username === un && user.password === pw).pop();
+                
+//                 if (!user) {
+//                     cb(new errors.AuthenticationError('Bad credentials provided.'));
+//                     return;
+//                 }
+                
+//                 cb(null, user);
+        
+//             }, 250);
+        
+//         }
+        
+//         const addNewUser = (newUser, cb) => {
+            
+//             if (!newUser) {
+//                 cb(new errors.BadRequestError('Falsy user object provided.'));
+//                 return;
+//             }
+        
+//             let invalid = !Object.keys(newUser).every(key => {
+//                 if(key == 'id') return true;
+//                 return newUser[key]
+//             });
+        
+//             if (invalid) {
+//                 cb(new errors.BadRequestError('Invalid property values found in provided user.'));
+//                 return;
+//             }
+        
+//             setTimeout(() => {
+        
+//                 let conflict = data.filter(user => user.username == newUser.username).pop();
+        
+//                 if (conflict) {
+//                     cb(new errors.ResourcePersistenceError('The provided username is already taken.'));
+//                     return;
+//                 }
+        
+//                 conflict = data.filter(user => user.email == newUser.email).pop();
+        
+//                 if (conflict) {
+//                     cb(new errors.ResourcePersistenceError('The provided email is already taken.'));
+//                     return;
+//                 }
+        
+//                 newUser.id = (data.length) + 1;
+//                 data.push(newUser);
+
+//                 mailWorker.emit('newRegister', newUser.email);
+        
+//                 cb(null, newUser);
+        
+//             });
+        
+//         }
+        
+//         const updateUser = (updatedUser, cb) => {
+        
+//             if (!updatedUser) {
+//                 cb(new errors.BadRequestError('Falsy user object provided.'));
+//                 return;
+//             }
+        
+//             if (!updatedUser.id) {
+//                 cb(new errors.BadRequestError('An id must be provided for update operations.'));
+//                 return;
+//             }
+        
+//             let invalid = !Object.values(updatedUser).every(val => val);
+        
+//             if (invalid) {
+//                 cb(new errors.BadRequestError('Invalid property values found in provided user.'));
+//                 return;
+//             }
+        
+//             setTimeout(() => {
+        
+//                 let persistedUser = data.find(user => user.id === updatedUser.id);
+        
+//                 if (!persistedUser) {
+//                     cb(new errors.ResourceNotFoundError('No user found with provided id.'))
+//                 }
+                
+//                 if (persistedUser.username != updatedUser.username) {
+//                     cb(new errors.ResourcePersistenceError('Usernames cannot be updated.'));
+//                     return;
+//                 }
+        
+//                 const conflict = data.filter(user => {
+//                     if (user.id == updatedUser.id) return false;
+//                     return user.email == updatedUser.email; 
+//                 }).pop();
+        
+//                 if (conflict) {
+//                     cb(new errors.ResourcePersistenceError('Provided email is taken by another user.'));
+//                     return;
+//                 }
+    
+//                 persistedUser = updatedUser;
+//                 cb(null, true);
+        
+//             });
+        
+//         }
+    
+//         return {
+//             getAllUsers,
+//             getUserById,
+//             getUserByUsername,
+//             getUserByCredentials,
+//             addNewUser,
+//             updateUser
+//         }
+
+//     }
+
+//     return {
+//         getInstance: function() {
+//             return !instance ? instance = init() : instance;
+//         }
+//     }
+
+// })();
