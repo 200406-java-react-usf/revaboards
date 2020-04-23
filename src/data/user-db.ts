dba96d1e56b6d4cf63f94ab09cecb0a6bb719493
wandeltk <wandeltk@gmail.com>
2020-04-23 10:54:15 -0600

    pulled from master

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -1,9 +1,16 @@
 import { User } from '../models/user';
+<<<<<<< HEAD:src/data/user-db.ts
 
 let id = 1;
 
 export default [
     new User(id++, 'aanderson', 'password', 'Alice', 'Anderson', 'aanderson@revature.com', '01/01/1990'),
+=======
+let id = 1;
+
+export default [
+    new User(id++, 'aanderson', 'password', 'Alice', 'Anderson', 'aanderson@revature.com', '01/01/1995'),
+>>>>>>> 8153c2805155dccefcc0668532ec580f9e013978:src/data/user-db.js
     new User(id++, 'bbailey', 'password', 'Bob', 'Bailey', 'bbailey@revature.com', '01/01/1983'),
     new User(id++, 'ccountryman', 'password', 'Charlie', 'Countryman', 'ccountryman@revature.com', '01/01/1990'),
     new User(id++, 'ddavis', 'password', 'Daniel', 'Davis', 'ddavis@revature.com', '07/01/1990'),
--- src/models/user.ts
+++ src/models/user.ts
@@ -9,11 +9,17 @@ export class User {
     dob: Date;
     age: number;
 
+<<<<<<< HEAD
     constructor(id: number, username: string, pw: string,
         fn: string, ln: string, email: string, dob: Date) {
 
         this.id = id;
         this.username = username;
+=======
+    constructor(id: number, un: string, pw: string, fn: string, ln: string, email: string, dob: Date) {
+        this.id = id;
+        this.username = un;
+>>>>>>> 8153c2805155dccefcc0668532ec580f9e013978
         this.password = pw;
         this.firstName = fn;
         this.lastName = ln;
@@ -22,5 +28,9 @@ export class User {
         this.age = Math.abs(new Date(Date.now() - this.dob.getTime()).getUTCFullYear() - 1970);
     }
 
+<<<<<<< HEAD
 
 }
+=======
+};
+>>>>>>> 8153c2805155dccefcc0668532ec580f9e013978
