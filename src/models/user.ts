4dfa21a88a90e57c1a4bd9bfb4bffbacb0d1c32d
neftaly polanco <polanconeftaly@gmail.com>
2020-04-24 14:19:28 -0400

    all tests completed

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,5 @@
 export class User {
 
-	id: number;
-	username: string;
-	password: string;
-	firstName: string;
-	lastName: string;
-	email: string;
-	dob: Date;
-	age: number;
-
-	constructor(id: number, un: string, pw: string, fn: string, ln: string, email: string, dob: Date) {
-		this.id = id;
-		this.username = un;
-		this.password = pw;
-		this.firstName = fn;
-		this.lastName = ln;
-		this.email = email;
-		this.dob = dob;  
-		this.age = Math.abs(new Date(Date.now() - this.dob.getTime()).getUTCFullYear() - 1970);    
-	}
-}
-=======
     id: number;
     username: string;
     password: string;
@@ -40,4 +19,3 @@ export class User {
     }
 
 };
->>>>>>> daf77b61dba2347995fae4e5003f00e0aa703e03
