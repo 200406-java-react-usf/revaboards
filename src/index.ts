01534c48393731991348b36e91689f1a6a14050d
Scotty Thoms <spthoms19@gmail.com>
2020-04-24 16:35:09 -0500

    moved user-router and post-router outside of index and into src/routers folder

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { PostRepository } from './repos/post-repo';
 import { UserRepository } from './repos/user-repo';
 
 import { UserRouter } from './routers/user-router';
+import { PostRouter } from './routers/post-router';
 
 const app = express();
 const postRepo = PostRepository.getInstance();
@@ -12,16 +13,7 @@ const postRepo = PostRepository.getInstance();
 app.use('/', bodyparser.json());
 
 app.use('/users', UserRouter);
-
-// app.get('/posts', async (req, resp) => {
-
-//     try{
-//         let payload = await postRepo.getAll();
-//         resp.status(200).json(payload).send();
-//     } catch (e){
-//         resp.status(404).json(e).send();
-//     }
-// });
+app.use('/posts', PostRouter);
 
 app.listen(8080, () => {
     console.log(`Application running and listening at: http://localhost:8080`);
