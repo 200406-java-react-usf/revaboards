869a8db7be5fbce95f1e544513a60eda308ff331
Scotty Thoms <spthoms19@gmail.com>
2020-04-24 16:34:36 -0500

    updated user-router and post-router to use routers and express

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -14,3 +14,13 @@ UserRouter.get('/', async (req, resp) => {
         resp.status(404).json(e).send();
     }
 });
+
+UserRouter.get('/:id', async (req, resp) => {
+    const id = +req.params.id; //the plus sign is to type coerce id into a number
+    try { 
+        let payload = await userRepo.getById(id);
+        resp.status(200).json(payload).send();
+    } catch (e) {
+        resp.status(404).json(e).send();
+    }
+})
