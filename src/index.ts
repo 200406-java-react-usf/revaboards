198f1b48aa5f1d640441d8377837d67499e9c8bb
Scotty Thoms <spthoms19@gmail.com>
2020-04-24 16:24:26 -0500

    changed index to use localhost 8080

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
+import express from 'express';
+import * as bodyparser from 'body-parser';
+
 import { PostRepository } from './repos/post-repo';
 import { UserRepository } from './repos/user-repo';
 
-(async function() {
+import { UserRouter } from './routers/user-router';
 
-    let userRepo = UserRepository.getInstance();
-    
-    console.log(await userRepo.getAll());
+const app = express();
+const postRepo = PostRepository.getInstance();
 
-})();
+app.use('/', bodyparser.json());
+
+app.use('/users', UserRouter);
+
+// app.get('/posts', async (req, resp) => {
+
+//     try{
+//         let payload = await postRepo.getAll();
+//         resp.status(200).json(payload).send();
+//     } catch (e){
+//         resp.status(404).json(e).send();
+//     }
+// });
+
+app.listen(8080, () => {
+    console.log(`Application running and listening at: http://localhost:8080`);
+});
