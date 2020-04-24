3b7c15d86a6632ea0baeb7c35aeb8d5114667976
wezley <wezley.singleton@gmail.com>
2020-04-24 00:57:20 -0400

    added express starter logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { PostRepository } from './repos/post-repo';
+import * as express from 'express';
+import * as bodyparser from 'body-parser';
+
 import { UserRepository } from './repos/user-repo';
+import { PostRepository } from './repos/post-repo';
 
-(async function() {
+const app = express();
+const userRepo = UserRepository.getInstance();
+const postRepo = PostRepository.getInstance();
 
-    let userRepo = UserRepository.getInstance();
-    
-    console.log(await userRepo.getAll());
+app.use('/', bodyparser.json());
 
-})();
+app.get('/users', async (req: express.Request, resp: express.Response) => {
+    try {
+        let payload = await userRepo.getAll();
+        resp.json(payload).send();
+    } catch (e) {
+        resp.status(400).send(JSON.stringify(e));
+    }
+});
+
+app.get('/posts', async (req: express.Request, resp: express.Response) => {
+    try {
+        let payload = await postRepo.getAll();
+        resp.json(payload).send();
+    } catch (e) {
+        resp.status(400).send(JSON.stringify(e));
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Application running and listening at: http://localhost:8080');
+})
