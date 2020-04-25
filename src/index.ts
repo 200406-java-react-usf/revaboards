f3ab3040de244bbcc27ab764abfdc7d1b74009ab
louRemy <louisremy96@gmail.com>
2020-04-24 20:46:16 -0400

    express demo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
+
+import express from 'express';
+
 import { PostRepository } from './repos/post-repo';
 import { UserRepository } from './repos/user-repo';
 
-(async function() {
 
-    let userRepo = UserRepository.getInstance();
+const app = express();
+
+const userRepo = UserRepository.getInstance();
+const postRepo = PostRepository.getInstance();
+
+
+app.get('/users', async (req, resp)=>{
+    try{
+    let payload = await userRepo.getAll();
+    resp.status(200).json(payload).send();
+    }catch (e){
+        resp.status(404).json(e)
+    }
+})    
     
-    console.log(await userRepo.getAll());
 
-})();
+app.get('/posts', async (req,resp)=>{
+    
+    try{let payload = await userRepo.getAll();
+    resp.status(200).json(payload).send
+    }catch(e){
+    resp.status(404).json(e);}
+})
+
+
+app.listen(8080, ()=>{
+    console.log("running and listening on port 8080")
+
+})
+
+
