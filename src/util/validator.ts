97b1d37fbd95e4368d024e1756c33fbd0116b5bd
wezley <wezley.singleton@gmail.com>
2020-04-26 01:11:48 -0400

    fixed: exports, isValidId; added: isPropertyOf
    kept default export for test code mocks, and also added individual function exports for cleanliness in source code use

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -1,12 +1,12 @@
-const isValidId = (id: number): boolean => {
-    return (id && typeof id === 'number' && Number.isInteger(id) && id > 0);
+export const isValidId = (id: number): boolean => {
+    return !!(id && typeof id === 'number' && Number.isInteger(id) && id > 0);
 };
 
-const isValidStrings = (...strs: string[]): boolean => {
+export const isValidStrings = (...strs: string[]): boolean => {
     return (strs.filter(str => !str || typeof str !== 'string').length == 0);
 };
 
-const isValidObject = (obj: Object, ...nullableProps: string[]) => {
+export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
     return obj && Object.keys(obj).every(key => {
         if (key === 'id') return isValidId(obj['id']);
         if (nullableProps.includes(key)) return true;
@@ -14,8 +14,25 @@ const isValidObject = (obj: Object, ...nullableProps: string[]) => {
     });
 };
 
+export const isPropertyOf = (prop: any, type: any) => {
+
+    console.log(`Property checker: ${prop}?`);
+
+    if (!prop || !type) {
+        return false;
+    }
+
+    let typeCreator = <T>(Type: (new () => T)): T => {
+        return new Type();
+    } 
+
+    return Object.keys(typeCreator(type)).includes(prop);
+
+}
+
 export default {
     isValidId,
     isValidStrings,
-    isValidObject
+    isValidObject,
+    isPropertyOf
 };
