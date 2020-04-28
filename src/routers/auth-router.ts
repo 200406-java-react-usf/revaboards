78e1aa9597f3ab298d84c926833639b5afdec539
wezley <wezley.singleton@gmail.com>
2020-04-28 00:09:51 -0400

    added auth router for login and logout endpoints

---
+++ src/routers/auth-router.ts
@@ -0,0 +1,30 @@
+import url from 'url';
+import express from 'express';
+import AppConfig from '../config/app';
+import { isEmptyObject } from '../util/validator';
+import { Principal } from '../dtos/principal';
+
+export const AuthRouter = express.Router();
+
+const userService = AppConfig.userService;
+
+AuthRouter.get('', (req, resp) => {
+    delete req.session.principal;
+    resp.status(200).send();
+});
+
+AuthRouter.post('', async (req, resp) => {
+
+    try {
+        const { username, password } = req.body;
+        let authUser = await userService.authenticateUser(username, password);
+        let payload = new Principal(authUser.id, authUser.username, authUser.role);
+        req.session.principal = payload;
+        resp.status(200).json(payload);
+    } catch (e) {
+        resp.status(e.statusCode || 500).json(e);
+    }   
+
+    resp.send();
+
+});
