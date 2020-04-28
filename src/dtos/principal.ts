b0cb4cf3fbd740fde1b3676a8b806d125afc540b
wezley <wezley.singleton@gmail.com>
2020-04-28 00:05:21 -0400

    added principal dto

---
+++ src/dtos/principal.ts
@@ -0,0 +1,13 @@
+export class Principal {
+
+    id: number;
+    username: string;
+    role: string;
+
+    constructor(id: number, username: string, role: string) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+    
+}
