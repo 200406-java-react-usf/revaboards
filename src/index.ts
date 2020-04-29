c0f1a33649d14128270aea27fd04c075e9c8da81
Abraham Selenke <slinky@Abraham.local>
2020-04-29 08:17:43 -0600

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import express, { Response } from 'express';
+import bodyparser from 'body-parser';
+
 import { PostRepository } from './repos/post-repo';
 import { UserRepository } from './repos/user-repo';
+import { User } from './models/user';
 
-(async function() {
+const app = express();
+const userRepo = UserRepository.getInstance();
+const postRepo = PostRepository.getInstance();
 
-    let userRepo = UserRepository.getInstance();
+app.use('/', bodyparser.json());
+
+app.get('/user', async (req, resp) => {
     
-    console.log(await userRepo.getAll());
+    try{
+        let payload = await userRepo.getAll();
+        resp.status(200).json(payload).send();
+    }catch (e) {
+        resp.status(404).json(e).send();
+    }
+})
 
-})();
+app.listen(8080, () => {
+    console.log('Application running and listening at http://localhost:8080');
+    
+})
