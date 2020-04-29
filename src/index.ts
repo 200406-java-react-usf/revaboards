fdc636cc962fb090f9694affcb34a30933ae456a
Scotty Thoms <spthoms19@gmail.com>
2020-04-29 09:53:09 -0500

    merged with master

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,4 @@
 import express from 'express';
-<<<<<<< HEAD
-import * as bodyparser from 'body-parser';
-
-import { PostRepository } from './repos/post-repo';
-import { UserRepository } from './repos/user-repo';
-
-import { UserRouter } from './routers/user-router';
-import { PostRouter } from './routers/post-router';
-
-const app = express();
-const postRepo = PostRepository.getInstance();
-
-app.use('/', bodyparser.json());
-
-app.use('/users', UserRouter);
-app.use('/posts', PostRouter);
-
-app.listen(8080, () => {
-    console.log(`Application running and listening at: http://localhost:8080`);
-});
-=======
 import fs from 'fs';
 import morgan from 'morgan';
 import path from 'path';
@@ -50,4 +29,3 @@ app.use('/auth', AuthRouter);
 app.listen(8080, () => {
     console.log(`Application running and listening at: http://localhost:8080`);
 });
->>>>>>> master
