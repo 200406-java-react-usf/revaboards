7ab49f53a0cfdb36e0e8a2f00778f6ee9f97fa72
Scotty Thoms <spthoms19@gmail.com>
2020-04-29 11:48:05 -0500

    fixed issue where POST request in auth was giving a 'cannot set headers' error

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -4,13 +4,11 @@ import { AuthenticationError, AuthorizationError } from "../errors/errors";
 export const adminGuard = (req: Request, resp: Response, next) => {
 
     if (!req.session.principal) {
-        //resp.sendStatus(401).send(new AuthenticationError('No session found! Please login.'));
-        resp.send(new AuthenticationError('No session found! Please login.'));
+        resp.status(401).send(new AuthenticationError('No session found! Please login.'));
     } else if (req.session.principal.role === 'Admin') {
         next();
     } else {
-        // resp.sendStatus(403).send(new AuthorizationError());
-        resp.send(new AuthorizationError('You are not an admin user.'));
+        resp.status(403).send(new AuthorizationError());
     }
 
 }
