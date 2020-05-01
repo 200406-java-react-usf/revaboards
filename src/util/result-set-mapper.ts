84fb6de36eda3d7a6cb95c64b8d976c8f69262b5
Drew Peters <peters.drew.dp@gmail.com>
2020-05-01 14:53:10 -0400

    updated w/ master, and updated to connect to db

--- src/util/result-set-mapper.ts
+++ src/util/result-set-mapper.ts
@@ -1,9 +1,5 @@
 import { UserSchema } from "./schemas";
 import { User } from "../models/user";
-<<<<<<< HEAD
-export function mapUserResultSet(resultSet: UserSchema) : User {
-    return new User (
-=======
 
 export function mapUserResultSet(resultSet: UserSchema): User {
     
@@ -12,7 +8,6 @@ export function mapUserResultSet(resultSet: UserSchema): User {
     }
 
     return new User(
->>>>>>> 19196196c63af8ae59422d7b966d2411bab8c80b
         resultSet.id,
         resultSet.username,
         resultSet.password,
