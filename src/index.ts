8c9139bb2d0820d70f2d5eb72ab0f5f05df99f3c
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-01 10:53:07 -0400

    added pg config and cleaned up formatting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import fs from 'fs';
 import morgan from 'morgan';
@@ -8,12 +9,29 @@ import { PostRouter } from './routers/post-router';
 import { AuthRouter } from './routers/auth-router';
 import { sessionMiddleware } from './middleware/session-middleware';
 import { corsFilter } from './middleware/cors-filter';
+import { Pool } from 'pg';
 
-const app = express();
+//environment configuraton
+
+dotenv.config();
+
+// database configuration
+
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
+});
 
 // logging configuration
 fs.mkdir(`${__dirname}/logs`, () => {});
 const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+
+//web server configuration
+const app = express();
 app.use(morgan('combined', { stream: logStream }));
 
 app.use('/', express.json());
